39c3b2325f8004242a12930f1607aa34ff988b2a
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2019-09-24 21:06:55 -0400

    fixing broken code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,27 +5,27 @@ const isAuthenticated = require("../config/middleware/isAuthenticated")
 
 module.exports = function(app){
 
-    // app.get("/", function(req, res){
+    app.get("/", function(req, res){
 
-    //     if(req.user){
-    //         res.redirect("/members");
-    //     }
-    //     res.sendFile(path.join(__dirname, "../public/login.html"))
-    // });
+        if(req.user){
+            res.redirect("/members");
+        }
+        res.sendFile(path.join(__dirname, "../public/login.html"))
+    });
 
-    // app.get("/signup", function(req,res){
+    app.get("/signup", function(req,res){
 
-    //     if(req.user){
-    //         res.redirect("/members");
-    //     }
-    //     res.sendFile(path.join(__dirname, "../public/signup.html"))
-    // })
+        if(req.user){
+            res.redirect("/members");
+        }
+        res.sendFile(path.join(__dirname, "../public/signup.html"))
+    })
 
-    app.get("/", function(req, res) {
+    app.get("/members", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/members.html"))
     })
 
-    app.get("/recipe", function(req, res) {
+    app.get("/recipe", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/recipe.html"))
     })
 
